a22d53bee5bef8ce738d5c7978e37fbed06c6411
Bartłomiej Flis <Bartekdawidflis@gmail.com>
2024-05-16 21:54:31 +0200

    add license to docstrings

--- service_manager/docker_manager.py
+++ service_manager/docker_manager.py
@@ -5,6 +5,22 @@ I didn’t want to use any external libraries in this part of the project.
 
 This module defines a DockerManager class to handle Docker operations like building images,
 managing container lifecycle, and monitoring container logs.
+LICENSE
+=======
+Copyright (C) 2024  Bartłomiej Flis
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import subprocess
 import sys
