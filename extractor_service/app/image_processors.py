f41349b7d47e7bf805caa35c880bc083edff8b64
Bartłomiej Flis <Bartekdawidflis@gmail.com>
2024-05-16 21:54:31 +0200

    add license to docstrings

--- extractor_service/app/image_processors.py
+++ extractor_service/app/image_processors.py
@@ -2,6 +2,22 @@
 This module provides abstract class for creating image processors and image processors.
 Image processors:
     - OpenCVImage: using OpenCV library to manage operations on images.
+LICENSE
+=======
+Copyright (C) 2024  Bartłomiej Flis
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import logging
 import uuid
--- extractor_service/app/schemas.py
+++ extractor_service/app/schemas.py
@@ -4,6 +4,22 @@ Models:
     - ExtractorConfig: Model containing the extractors configuration parameters.
     - Message: Model for encapsulating messages returned by the application.
     - ExtractorStatus: Model representing the status of the currently working extractor in the system.
+LICENSE
+=======
+Copyright (C) 2024  Bartłomiej Flis
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import logging
 from pathlib import Path
