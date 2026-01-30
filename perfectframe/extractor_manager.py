d5ae50f763c8b402d21e24df83f52b9efe532bd6
BKDDFS <Bartekdawidflis@gmail.com>
2026-01-30 17:08:37 +0100

    chore: change license from GPL v3 to Apache 2.0
    - Replace LICENSE.md with Apache License 2.0 text
    - Remove GPL license blocks from Python module docstrings
    - Update README license reference and add v3.0 to comparison table
    - Remove license badge (GitHub displays it automatically)

--- perfectframe/extractor_manager.py
+++ perfectframe/extractor_manager.py
@@ -1,22 +1,4 @@
-"""Provide manager class for running extractors and managing extraction process lifecycle.
-
-LICENSE
-=======
-Copyright (C) 2024  Bartłomiej Flis
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
+"""Provide manager class for running extractors and managing extraction process lifecycle."""
 
 import logging
 import threading
